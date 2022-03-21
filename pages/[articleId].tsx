60d7c2792b03accd0666f461cec2178aa4850578
futurouz <kunanan.tw@gmail.com>
2022-03-21 10:11:09 +0700

    Add basic style to article page

--- pages/[articleId].tsx
+++ pages/[articleId].tsx
@@ -1,18 +1,50 @@
 import { fetchSingleArticle } from '../actions'
+import Layout from '../components/Layout'
+import styles from '../styles/Article.module.css'
 
 export default function ArticleDetails(props: {
   docs: Awaited<ReturnType<typeof fetchSingleArticle>>
   isError: boolean
 }) {
   if (props.isError) {
-    return 'Cannot retrieve your article Please try again later.'
+    return <div>Cannot retrieve your article. Please try again later.</div>
   }
 
+  const {
+    abstract,
+    headline: { main },
+    byline: { original },
+    lead_paragraph,
+    section_name,
+    web_url,
+    pub_date,
+  } = props.docs[0]
+
   return (
-    <div>
-      <h1>{props.docs[0].headline.main}</h1>
-      <p>{props.docs[0].lead_paragraph}</p>
-    </div>
+    <Layout>
+      <div className={styles.article__container}>
+        <div className={styles.article__top_content_container}>
+          <p className={styles.article__section_name}>{section_name}</p>
+          <h1 className={styles.article__header}>{main}</h1>
+          <h2 className={styles.article__abstract}>{abstract}</h2>
+        </div>
+
+        <div className={styles.article__original}>{original}</div>
+        <time className={styles.article__pub_date} dateTime={pub_date}>
+          Published: {new Date(pub_date).toDateString()}
+        </time>
+        <p className={styles.article__lead_paragraph}>{lead_paragraph}</p>
+
+        <a
+          className={styles.article__web_url}
+          href={web_url}
+          target="_blank"
+          rel="noreferrer"
+        >
+          Further reading
+        </a>
+      </div>
+    </Layout>
   )
 }
 
