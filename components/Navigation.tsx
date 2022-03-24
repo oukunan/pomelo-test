88c39e386a0c1b66dcf26b78721a779f5e582757
futurouz <kunanan.tw@gmail.com>
2022-03-24 11:25:42 +0700

    Update Navigation component style

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,18 +1,38 @@
+import cx from 'classnames'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import React from 'react'
 
 import styles from './Navigation.module.css'
 
 export default React.memo(function Navigation() {
+  const router = useRouter()
+
+  const navigationOptions = [
+    {
+      path: '/',
+      content: 'Home',
+    },
+    {
+      path: '/about',
+      content: 'About',
+    },
+  ]
+
   return (
-    <header className={styles.navigation_header}>
-      <ul className={styles.navigation_container}>
-        <li className={styles.navigation_item}>
-          <Link href="/">Home</Link>
-        </li>
-        <li className={styles.navigation_item}>
-          <Link href="/about">About</Link>
-        </li>
+    <header className={styles.navigation__header}>
+      <ul className={styles.navigation__container}>
+        {navigationOptions.map((option) => (
+          <li
+            key={option.path}
+            className={cx(
+              styles.navigation__item,
+              router.pathname === option.path && styles.navigation__item_active
+            )}
+          >
+            <Link href={option.path}>{option.content}</Link>
+          </li>
+        ))}
       </ul>
     </header>
   )
